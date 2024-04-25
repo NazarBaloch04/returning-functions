54bd2d39a3e190cc31a91a0b36e30e1084becae9
NazarBaloch04 <153612842+NazarBaloch04@users.noreply.github.com>
2024-04-25 16:29:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+//  function that returns a string that has letters in alphabetical order.
+// Example string : 'Pakistan'
+// Expected Output : 'aaiknPst'
+// Assume punctuation and numbers symbols are not included in the passed string.
+// Define a function named alphabet_order that takes a string parameter (str)
+function alphabet_order(str) {
+    // Split the string into an array of characters, sort the array, and join the characters back into a string
+    return str.split('').sort().join('');
+}
+// Log the result of calling alphabet_order with the input string "webmaster" to the console
+console.log(alphabet_order("pakistan"));
+// The split() method is used to split a String object into an array of strings by separating the string into substrings.
+// Code : console.log('pakistan'.split(""));
+// Output : ["p", "a", "k", "i", "s" , "t" , "a", "n"]
+// The sort() method is used to sort the elements of an array in place and returns the array.
+// Code : console.log(["p", "a", "k", "i", "s" , "t" , "a", "n"].sort());
+// Output : ["a", "a", "i", "k", "n" , "p" , "s", "t"]
+// The join() method is used to join all elements of an array into a string.
+// Code : console.log(["a", "a", "i", "k", "n" , "p" , "s", "t"].join(""));
+// Output : "aaiknpst"
+function country(str) {
+    if (str === void 0) { str = "pakistan"; }
+    // Split the string into an array of characters, sort the array, and join the characters back into a string
+    return str;
+}
+// Log the result of calling alphabet_order with the input string "webmaster" to the console
+console.log(country());
+function Country(str, str1) {
+    if (str === void 0) { str = "pakistan"; }
+    if (str1 === void 0) { str1 = "England"; }
+    // Split the string into an array of characters, sort the array, and join the characters back into a string
+    return "".concat(str, " ").concat(str1);
+}
+// Log the result of calling alphabet_order with the input string "webmaster" to the console
+console.log(Country());
+function Admission(name, Qualification, email) {
+    console.log("my name is ".concat(name, " i am ").concat(Qualification, " my email is ").concat(email));
+}
+Admission("Nazar Baloch", "under  Graguated", "mazarinazarali04@gmail.com");
+function weatherCondition(temperature) {
+    if (temperature === void 0) { temperature = "23 Celcius"; }
+    return "Today the temperature will rise till ".concat(temperature);
+}
+console.log(weatherCondition());
